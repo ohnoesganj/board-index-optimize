bbab875d217fdf8f3e022b507b719fc11b3e5b0e
ohnoesganj <sunho3456@naver.com>
2025-04-22 11:13:02 +0900

    First Commit

---
+++ src/main/kotlin/org/example/boardoptimize/BoardOptimizeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.boardoptimize
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardOptimizeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardOptimizeApplication>(*args)
+}
